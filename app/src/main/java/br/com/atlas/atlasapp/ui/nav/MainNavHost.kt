0568215c645b96194ae6539f32c9ed37d66b9be3
DavidEsdrs <carpina1983@gmail.com>
2026-05-26 05:55:01 -0300

    feat: inicializa projeto

---
+++ app/src/main/java/br/com/atlas/atlasapp/ui/nav/MainNavHost.kt
@@ -0,0 +1,23 @@
+package com.example.weatherapp.ui.nav
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.weatherapp.model.MainViewModel
+import com.example.weatherapp.ui.HomePage
+import com.example.weatherapp.ui.ListPage
+import com.example.weatherapp.ui.MapPage
+
+@Composable
+fun MainNavHost(navController: NavHostController,
+                viewModel: MainViewModel,
+                modifier: Modifier = Modifier
+) {
+    NavHost(navController, startDestination = Route.Home) {
+        composable<Route.Home> { HomePage(modifier = modifier, viewModel = viewModel) }
+        composable<Route.List> { ListPage(modifier = modifier, viewModel = viewModel) }
+        composable<Route.Map> { MapPage(modifier = modifier, viewModel = viewModel) }
+    }
+}
